4fc67d235d8cc12f0b0ec0dd1d3abd6ad50b941d
Giulio Cervera <giulio.cervera@gmail.com>
2012-10-20 19:11:53 +0200

    Provide permission for graphics to set service.bootanim.exit prop
    Change-Id: If23d14172022907fcefdcf7430904eb560d4d64c

--- init/property_service.c
+++ init/property_service.c
@@ -102,6 +102,7 @@ struct {
     { "persist.service.bdroid.", AID_BLUETOOTH,   0 },
     { "selinux."         , AID_SYSTEM,   0 },
     { "net.pdp",          AID_RADIO,    AID_RADIO },
+    { "service.bootanim.exit", AID_GRAPHICS, 0 },
 #ifdef PROPERTY_PERMS_APPEND
 PROPERTY_PERMS_APPEND
 #endif
